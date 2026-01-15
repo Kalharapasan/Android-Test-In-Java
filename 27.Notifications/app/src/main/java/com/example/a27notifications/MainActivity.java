ed0828a1e5bb9eed229ef88eb14743263aab7411
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-15 10:53:27 +0530

    Notifications

--- 27.Notifications/app/src/main/java/com/example/a27notifications/MainActivity.java
+++ 27.Notifications/app/src/main/java/com/example/a27notifications/MainActivity.java
@@ -5,19 +5,40 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
+
     Button btn;
     String CHANNEL_ID = "my_channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.button);
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{android.Manifest.permission.POST_NOTIFICATIONS},
+                        101
+                );
+            }
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                     CHANNEL_ID,
@@ -31,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(MainActivity.this, MainActivity.class);
 
                 PendingIntent pendingIntent = PendingIntent.getActivity(
@@ -39,19 +61,28 @@ public class MainActivity extends AppCompatActivity {
                         intent,
                         PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
                 );
-                Notification notification = null;
+                Notification notification;
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     notification = new Notification.Builder(MainActivity.this, CHANNEL_ID)
-                            .setTicker("TickerTile")
-                            .setContentTitle("Content title")
-                            .setContentText("Conten Text Hi I'm Kalhara")
                             .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle("Content Title")
+                            .setContentText("Hi I'm Kalhara")
                             .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .build();
+                } else {
+                    notification = new Notification.Builder(MainActivity.this)
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle("Content Title")
+                            .setContentText("Hi I'm Kalhara")
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
                             .build();
                 }
-                notification.flags = Notification.FLAG_AUTO_CANCEL;
+
                 NotificationManager notificationManager =
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
                 notificationManager.notify(1, notification);
             }
         });
