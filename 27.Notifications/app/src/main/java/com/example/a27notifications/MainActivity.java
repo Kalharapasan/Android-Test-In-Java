d8d17c03077890891257f97f5d5d0ef4a7338432
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-15 10:44:42 +0530

    Notifications

--- 27.Notifications/app/src/main/java/com/example/a27notifications/MainActivity.java
+++ 27.Notifications/app/src/main/java/com/example/a27notifications/MainActivity.java
@@ -1,24 +1,59 @@
 package com.example.a27notifications;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    String CHANNEL_ID = "my_channel";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn = findViewById(R.id.button);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "My Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+        }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        MainActivity.this,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                );
+                Notification notification = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    notification = new Notification.Builder(MainActivity.this, CHANNEL_ID)
+                            .setTicker("TickerTile")
+                            .setContentTitle("Content title")
+                            .setContentText("Conten Text Hi I'm Kalhara")
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentIntent(pendingIntent)
+                            .build();
+                }
+                notification.flags = Notification.FLAG_AUTO_CANCEL;
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(1, notification);
+            }
         });
     }
 }
