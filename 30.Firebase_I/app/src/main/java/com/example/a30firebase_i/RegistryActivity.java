37e4331923000bbb0dae0c3f5152652aebebb240
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-15 17:24:58 +0530

    Firebase User Registration  Authentication

--- 30.Firebase_I/app/src/main/java/com/example/a30firebase_i/RegistryActivity.java
+++ 30.Firebase_I/app/src/main/java/com/example/a30firebase_i/RegistryActivity.java
@@ -1,21 +1,30 @@
 package com.example.a30firebase_i;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegistryActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class RegistryActivity extends AppCompatActivity {
     private Button regiButton;
     private EditText fName,lname,email,password;
-
-
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,38 @@ public class RegistryActivity extends AppCompatActivity {
         lname=findViewById(R.id.etFirstName);
         email =findViewById(R.id.etEmail);
         password =findViewById(R.id.etPassword);
+        auth =FirebaseAuth.getInstance();
 
+        regiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String f_name = fName.getText().toString();
+                String l_name = lname.getText().toString();
+                String _password = password.getText().toString();
+                String _email = email.getText().toString();
+                if (TextUtils.isEmpty(f_name)||TextUtils.isEmpty(l_name)||TextUtils.isEmpty(_email)||TextUtils.isEmpty(_password)){
+                    Toast.makeText(RegistryActivity.this, "Empty Credentioal", Toast.LENGTH_SHORT).show();
+                }else if(_password.length() <6){
+                    Toast.makeText(RegistryActivity.this, "Password too short", Toast.LENGTH_SHORT).show();
+                }else{
+                    registerUser(f_name,l_name,_email,_password);
+                }
+            }
+            private void registerUser(String fName, String lName, String email, String password) {
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegistryActivity.this,
+                        new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegistryActivity.this, "User Registry Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistryActivity.this,LoginActivity.class));
+                            finish();
+                        }else{
+                            Toast.makeText(RegistryActivity.this, "User Registry Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
